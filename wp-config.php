5ae94ccbcca42b80a2a35b0f2bed5b7aa4edc210
r03eRt <morgadoluengo@gmail.com>
2017-03-30 00:21:58 +0200

    added pages and ACF for each language

--- wp-config.php
+++ wp-config.php
@@ -20,6 +20,8 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
+//define('WP_CACHE', true); //Added by WP-Cache Manager
+define( 'WPCACHEHOME', '/Users/robertomorgadoluengo/work/code/meraki/wp-content/plugins/wp-super-cache/' ); //Added by WP-Cache Manager
 define('DB_NAME', 'meraki');
 
 /** MySQL database username */
