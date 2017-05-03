9ab04f2aebb0de45333dcd5c4a87a6f0f34666e3
r03eRt <morgadoluengo@gmail.com>
2017-05-03 20:46:38 +0200

    added coming soon page

--- wp-content/themes/stockholm/footer.php
+++ wp-content/themes/stockholm/footer.php
@@ -323,5 +323,139 @@ if(is_array($footer_classes_array) && count($footer_classes_array)) {
 </div>
 </div>
 <?php wp_footer(); ?>
+
+<?php if(get_field('coming_soon', 'options')){ ?>
+<style>
+@font-face {
+  font-family: "merakistudiomadrid";
+  font-style: normal;
+  font-weight: normal;
+  src: url("http://www.merakistudiomadrid.com/css/fonts/futura_lt_book_1-.eot") format("eot"), url("http://www.merakistudiomadrid.com/css/fonts/futura_lt_book_1.woff2") format("ttf"), url("http://www.merakistudiomadrid.com/css/fonts/futura_lt_book_1.woff") format("woff"); }
+
+#overlay-coming-soon {
+    background: transparent url(http://www.merakistudiomadrid.com/img/fondo.jpg) 0 0 no-repeat;
+    background-size: cover;
+    margin: 0;
+    padding: 0;
+    min-width: 100vw;
+    min-height: 100vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 99999;
+    overflow: hidden;
+}
+
+html {
+	margin: 0!important;
+}
+
+footer {
+	display: none;
+}
+
+#overlay-coming-soon .page section.brand {
+  width: 320px;
+  padding-left: 40px;
+  padding-right: 40px;
+  max-width: 100%;
+  margin-left: 63%;
+  text-align: center;
+  border: 0px solid rgba(255, 255, 255, 0.47);
+  background-color: rgba(255, 255, 255, 0.88);
+  min-height: 100vh; }
+  #overlay-coming-soon .page section.brand h1 {
+    padding-top: 160px;
+    margin-top: 0; }
+  #overlay-coming-soon .page section.brand .logo {
+    width: 169px;
+    margin-bottom: 40px; }
+  #overlay-coming-soon .page section.brand span.red {
+    color: #9A2921;
+    font-family: merakistudiomadrid,sans-serif;
+    font-size: 25px;
+    font-weight: bold; }
+  #overlay-coming-soon .page section.brand p {
+    color: #414141;
+    font-family: merakistudiomadrid,sans-serif; }
+  #overlay-coming-soon .page section.brand a {
+    display: block;
+    font-size: 13px;
+    color: #414141;
+    text-decoration: none;
+    line-height: 31px; }
+    #overlay-coming-soon .page section.brand a:hover {
+      text-decoration: underline; }
+  #overlay-coming-soon .page section.brand .social {
+    padding-bottom: 40px; }
+    #overlay-coming-soon .page section.brand .social .mail {
+      font-size: 10px; }
+    #overlay-coming-soon .page section.brand .social .iconos a {
+      display: inline-block;
+      transition: opacity 0.5s ease; }
+      #overlay-coming-soon .page section.brand .social .iconos a .socialIcon {
+        width: 39px; }
+      #overlay-coming-soon .page section.brand .social .iconos a:hover {
+        opacity: 0.5; }
+
+@media screen and (max-width: 768px) {
+  #overlay-coming-soon .page section.brand {
+    margin-left: 0;
+    margin: 0 auto;
+    padding-left: 10px;
+    padding-right: 10px; }
+    #overlay-coming-soon .page section.brand h1 {
+      padding-top: 10px;
+      margin-bottom: 10px; }
+    #overlay-coming-soon .page section.brand .logo {
+      margin-bottom: 0px; }
+    #overlay-coming-soon .page section.brand span.red {
+      font-size: 21px; }
+    #overlay-coming-soon .page section.brand .social {
+      padding-bottom: 10px; } }
+
+	
+</style>
+<div id="overlay-coming-soon">
+	<div class="page">
+		<section class="brand">
+			<h1 class="title2">
+				<a href="www.merakistudiomadrid.com">
+					<img class="logo" src="http://www.merakistudiomadrid.com/img/meraki-studio-madrid-logo.png" alt="MERAKI Studio Madrid">
+				</a>
+			</h1>
+
+			<span class="red">
+				COMING SOON
+			</span>
+
+			<p>
+				Estamos de obras en nuestra web.
+				Para más información no dudes en
+				escribirnos a:
+				<a href="mailto:​hola@merakistudiomadrid.com" class="mail">​hola@merakistudiomadrid.com</a>
+				 
+				o llamar al:
+				<a href="tel:660080141" class="tel">(34) 660 080 141</a>
+			</p>
+
+
+			<div class="social">
+				<!-- <a target="_blank" href="mailto:​hola@merakistudiomadrid.com" class="mail">​hola@merakistudiomadrid.com</a> -->
+				<div class="iconos">
+					<a href="https://www.instagram.com/merakistudiomadrid/" target="_blank" class="instagramLink">
+						<img src="http://www.merakistudiomadrid.com/img/instagram.png" alt="Instagram Meraki" class="instagram socialIcon">
+					</a>
+					<a href="https://www.facebook.com/merakistudiomadrid/?ref=aymt_homepage_panel" target="_blank" class="facebookLink">
+						<img src="http://www.merakistudiomadrid.com/img/facebook.png" alt="Facebook Meraki" class="facebook socialIcon">
+					</a>
+				</div>
+			</div>
+
+		</section>
+	</div>
+</div>
+<?php } ?>
+
 </body>
 </html>
